76cc99008e44087d84833c59d2a2dfda4a04dc6c
Maksim Botalov <dev.botalov@gmail.com>
2022-11-19 04:55:58 +0500

    Initial Commit

---
+++ MoneyControl/MoneyControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyControlApp.swift
+//  MoneyControl
+//
+//  Created by Максим Боталов on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
